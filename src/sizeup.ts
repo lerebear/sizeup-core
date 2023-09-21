257f019188844d122b0f35c8c0fe2c08bc226080
Lere Williams <lerebear@github.com>
2023-09-20 23:19:33 -0500

    Simplify interface

--- src/sizeup.ts
+++ src/sizeup.ts
@@ -5,33 +5,25 @@ import * as YAML from "yaml"
 import * as fs from "fs"
 import { DefaultConfiguration } from "./config/default";
 
+const GITHUB_PULL_REQUEST_URL_RE = /https:\/\/github\.com\/([^/]+)\/([^/]+)\/pull\/(\d+)/
+
 export default class SizeUp {
-  /**
-   * Evaluates a diff for reviewability.
-   *
-   * @param diff a diff to evaluate
-   * @param configFile path to a configuration file containing options for how to evaluate the pull request
-   * @param client authenticated Octokit client that we should use to communicate with the GitHub API
-   */
-  static async evaluate(diff: string, configFile: string): Promise<Score>;
-
-  /**
-   * Evaluates a pull request for reviewability.
-   *
-   * @param url GitHub pull request URL denoting the pull request to evaluate
-   * @param configFile path to a configuration file containing options for how to evaluate the pull request
-   * @param client authenticated Octokit client that we should use to communicate with the GitHub API
-   */
-  static async evaluate(url: string, configFile: string, client: Octokit): Promise<Score>;
-
   /**
    * Evaluates some code (identified either by diff or a pull request URL) for reviewability.
    *
-   * @param diff_or_url Either a diff or a GitHub pull request URL denoting the code to evaluate
-   * @param configFile path to a configuration file containing options for how to evaluate the pull request
-   * @param client authenticated Octokit client that we should use to communicate with the GitHub API
+   * @param diff_or_url Either a diff or a GitHub pull request URL denoting the code to evaluate.
+   * @param client Authenticated Octokit client that we should use to communicate with the GitHub
+   *   API. This must be provided if a URL is passed via the `diff_or_url` parameter.
+   * @param configFile Path to a configuration file containing options for how to evaluate the pull
+   *   request.
    */
-  static async evaluate(diff_or_url: string, configFile: string, client?: Octokit): Promise<Score> {
+  static async evaluate(diff_or_url: string, client?: Octokit, configFile?: string): Promise<Score> {
+    const wasGivenUrl = diff_or_url.match(GITHUB_PULL_REQUEST_URL_RE)
+
+    if (!client && wasGivenUrl) {
+      throw new Error("`client` parameter must be provided with a URL")
+    }
+
     const config = configFile ? YAML.parse(fs.readFileSync(configFile, "utf8")) : {}
     const defaultConfig = DefaultConfiguration
     const ignored = config.ignored || defaultConfig.ignored
@@ -39,7 +31,7 @@ export default class SizeUp {
     const categories = config.categories || defaultConfig.categories
 
     let changeset: Changeset
-    if (client) {
+    if (client && wasGivenUrl) {
       changeset = await Changeset.fetch(diff_or_url, ignored, client)
     } else {
       changeset = new Changeset(diff_or_url, ignored)
