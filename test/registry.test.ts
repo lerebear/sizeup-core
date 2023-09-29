0bad5f72af3250a0a7104b37105a253efb04b0f4
Lere Williams <lerebear@github.com>
2023-09-28 22:07:43 -0700

    Add registry test

---
+++ test/registry.test.ts
@@ -0,0 +1,13 @@
+import { expect } from "chai"
+import { FeatureRegistry } from "../src/registry"
+
+describe("FeatureRegistry", () => {
+  describe("get", () => {
+    it("should contain the features used in the default formula", () => {
+      expect(FeatureRegistry.get("additions")).to.exist
+      expect(FeatureRegistry.get("deletions")).to.exist
+      expect(FeatureRegistry.get("comments")).to.exist
+      expect(FeatureRegistry.get("whitespace")).to.exist
+    })
+  })
+})
