9012d0eab79635d4eb95131c88505e108ba3132b
Lérè Williams <lere.williams@gmail.com>
2026-03-27 10:51:42 -0700

    Update eslint and use latest config format

--- test/helpers/init.ts
+++ test/helpers/init.ts
@@ -1,3 +1,5 @@
-const path = require('path')
+import * as path from 'path';
+import * as process from 'process';
+
 process.env.TS_NODE_PROJECT = path.resolve('test/tsconfig.json')
 process.env.NODE_ENV = 'development'
